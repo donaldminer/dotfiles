12673bb913f77e438e684f2920a585a54f7f8c09
donaldminer <drminer@dons.usfca.edu>
2024-09-04 07:58:37 -0700

    Adding some missing files

--- nvim/.config/nvim/lua/donminer/core/options.lua
+++ nvim/.config/nvim/lua/donminer/core/options.lua
@@ -2,7 +2,7 @@ vim.cmd("let g:netrw_liststyle=3")
 
 local opt = vim.opt
 
---opt.relativenumber = true
+opt.relativenumber = true
 opt.number = true
 
 -- Tabs & Indentation
@@ -12,7 +12,7 @@ opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
 
 -- Line Wrapping
-opt.wrap = false
+opt.wrap = true
 
 -- Search Settings
 opt.ignorecase = true -- ignore case when searching
